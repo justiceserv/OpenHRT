0e97272f3d0e2c78bc21c83816aefec645ceacd6
Justiceserv <justiceserv@justiceserv.dev>
2020-09-21 09:34:09 +0900

    Update uptimerobot.js

--- command/uptimerobot.js
+++ command/uptimerobot.js
@@ -1,1 +1,24 @@
 const Discord = require('Discord.js');
+const Config = require('/config/config.json');
+module.exports=
+{
+    message:function(argument)
+    {
+        if(argument === "온")
+        {
+            var OnEmbed = new Discord.MessageEmbed()
+                .setColor(`${Config.embedcolor}`)
+                .setTitle('UptimeRobot Configuration')
+                .setAuthor('OpenHRT', '', '')
+                .setDescription('UptimeRobot 알림이 꺼졌습니다!'); 
+        }
+        else if(argument === "오프")
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
